ffc6f32a04c05cff703161bb008d2578a2356ead
AkshaySharma777 <jsakshay653@gmail.com>
2023-10-19 21:06:20 +0530

    completed analog clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,13 @@
-function round(){
-    const canvas = document.getElementById('myCanvas');
-    const context = canvas.getContext('2d');
-    const centerX = canvas.width / 2;
-    const centerY = canvas.height / 2;
-    const radius = 70;
-    
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = 'green';
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = '#003300';
-    context.stroke();
-}
-round();
+setInterval(() => {
+  d = new Date();
+  htime = d.getHours();
+  mtime = d.getMinutes();
+  stime = d.getSeconds();
+  hrotation = 30*htime + mtime/2;
+  mrotation = 6*mtime;
+  srotation = 6*stime;
+
+  hour.style.transform = `rotate(${hrotation}deg)`;
+  minute.style.transform = `rotate(${mrotation}deg)`;
+  second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
